e44574c69bce74322a0061efeffb2f518547722e
MizanRbf <mizanrbf@gmail.com>
2025-03-10 17:42:33 +0600

    practice-3 finished

--- 004_JS_If-else/Practice-3.js
+++ 004_JS_If-else/Practice-3.js
@@ -15,4 +15,78 @@ else{
 
 // ******************************
 
-const 
+const age = 50;
+if(age < 12){
+console.log('free');
+}
+else if(age > 60){
+console.log('discount 50%');
+}
+else{
+  console.log('full pay');
+}
+
+// *****************************
+
+const bankBalance = 7000;
+if(bankBalance < 1000){
+  console.log('diposit kor');
+}
+else if(bankBalance < 5000){
+  console.log('Bindas life');
+}
+else{
+  console.log('donate more and more');
+}
+
+// **************************
+
+const marks = 90;
+if(marks < 50){
+  console.log('Fail');
+}
+else if(marks <= 80){
+  console.log('pass');
+}
+else{
+  console.log('A+');
+}
+
+// ********************
+
+const page = 100;
+if(page < 100){
+console.log('small book')
+}
+else if(page < 500){
+console.log('mid size book')
+}
+else{
+  console.log('heart attack size book')
+}
+
+// **************************
+
+const temperature = 40;
+if(temperature < 0){
+  console.log('ice');
+}
+else if(temperature < 20){
+  console.log('cool cool');
+}
+else{
+  console.log('Hot Hot');
+}
+
+// **********************************
+
+const level = 10;
+if(level < 10){
+console.log('novice');
+}
+else if(level < 50){
+console.log('expert');
+}
+else{
+  console.log('Pro Gamer');
+}
